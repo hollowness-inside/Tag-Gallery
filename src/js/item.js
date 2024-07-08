7bc46ad5527aa8d67092c5a208647af9ef265133
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-07-08 17:18:52 +1000

    Add docstrings

--- src/js/item.js
+++ src/js/item.js
@@ -1,4 +1,10 @@
 export class Item {
+    /**
+     * 
+     * @param {string} path 
+     * @param {string[]} tags 
+     * @param {Element} element 
+     */
     constructor(path, tags, element) {
         this.path = path;
         this.tags = tags;

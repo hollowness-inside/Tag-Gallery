6f56592a3df42bc947e318bc270ad4246229760a
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-07-08 14:44:44 +1000

    Implement clear filters logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+const navbar = document.getElementById('navbar');
+const taglist = document.getElementById('taglist');
+const viewport = document.getElementById('viewport');
+
+document.getElementById('clear').addEventListener('click', () => {
+    let tags = taglist.getElementsByTagName('input');
+
+    for (let element of tags)
+        element.checked = false;
+});

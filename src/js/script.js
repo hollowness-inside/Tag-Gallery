bddb16d9b6cc6f7f96dde2f025f1a0919968ebcb
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-07-08 17:29:45 +1000

    Add tag counts

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,40 @@
 import { JsonTagFS } from "../jfs.js";
 
 function updateViewport() {
+    const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+
     let checkboxes = taglist.getElementsByTagName("input");
     checkboxes = Array.from(checkboxes);
 
     const activeFilters = checkboxes.filter(ch => ch.checked).map(ch => ch.name);
     let filteredFiles = tagfs.filter(activeFilters);
 
+    let tag_counts = {};
+    tagfs.tags.forEach(tag => tag_counts[tag] = 0);
+
     tagfs.files.forEach(file => {
-        if (filteredFiles.includes(file))
+        if (filteredFiles.includes(file)) {
             file.element.style.display = "block";
-        else
+            file.tags.forEach(tag => {
+                tag_counts[tag] += 1;
+            });
+        } else {
             file.element.style.display = "none";
+        }
+    });
+
+    zip(Object.keys(tag_counts), Object.values(tag_counts)).forEach(([tag, count]) => {
+        let label = document.getElementById('label_' + tag);
+
+        if (count === 0) {
+            label.parentElement.style.display = 'none';
+            return;
+        } else {
+            label.parentElement.style.display = 'block';
+        }
+
+        let i = label.innerText.search(/\(\d+\)$/);
+        label.innerText = label.innerText.substring(0, i) + ' (' + count + ')';
     });
 }
 
@@ -25,7 +48,7 @@ function addTag(tag) {
     let label = document.createElement("label");
     label.setAttribute("for", checkbox.id);
     label.setAttribute("id", "label_" + tag);
-    label.innerText = tag;
+    label.innerText = tag + `(0)`;
 
     let listItem = document.createElement("li");
     listItem.appendChild(checkbox);

f5ae08b5a6904e7e75a8022f94a4fe43334f5b2c
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-07-19 11:47:56 +1000

    Introduce database

--- server.go
+++ server.go
@@ -1,49 +1,82 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+var db *sql.DB
 
 type Item struct {
 	Path string   `json:"path"`
 	Tags []string `json:"tags"`
 }
 
+func initDB(path string) (*sql.DB, error) {
+	conn, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = conn.Exec(`CREATE TABLE IF NOT EXISTS vault (
+                name STR(128) NOT NULL PRIMARY KEY,
+                path TEXT NOT NULL,
+                type STR(5),
+                tags TEXT);`); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
 func fetchItems(w http.ResponseWriter, r *http.Request) {
-	println("Works")
 	if r.Method != "GET" {
 		return
 	}
 
-	var item []Item
-	item = append(item, Item{
-		Path: "images/adam-bignell-BTrpO01nqK8-unsplash.jpg",
-		Tags: []string{"architecture", "sky"},
-	})
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM vault").Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	item = append(item, Item{
-		Path: "images/aedrian-salazar-7gBwDz_9AzI-unsplash.jpg",
-		Tags: []string{
-			"plant",
-			"nature",
-			"sky",
-		},
-	})
+	items := make([]Item, 0, count)
+	rows, err := db.Query("SELECT path, tags FROM vault")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	jitems, _ := json.Marshal(item)
+	for rows.Next() {
+		var path, jtags string
+		rows.Scan(&path, &jtags)
+
+		var tags []string
+		json.Unmarshal([]byte(jtags), &tags)
+
+		item := Item{path, tags}
+		items = append(items, item)
+	}
+
+	jitems, _ := json.Marshal(items)
 	w.Write(jitems)
 }
 
 func main() {
+	db, err := initDB("vault.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	fs := http.FileServer(http.Dir("./src"))
 	http.Handle("/", fs)
-
 	http.HandleFunc("/fetch", fetchItems)
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	if err = http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }

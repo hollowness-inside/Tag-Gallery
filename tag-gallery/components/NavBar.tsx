29946cfcc4dad56ca15fc99908bdf0251a557ecf
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-12-03 22:24:49 +1000

    Update

---
+++ tag-gallery/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button";
+
+const NavBar = () => {
+    return (
+        <div>
+            <Button>Clear Filters</Button>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ tag-gallery/components/Viewport.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Viewport = () => {
+    return (
+        <div>
+            Viewport
+        </div>
+    )
+}
+
+export default Viewport
